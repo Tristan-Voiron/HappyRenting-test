472e377f58bd6ba729e21725af930037a2648b23
Tristan-Voiron <tristan.voiron@gmail.com>
2020-11-18 08:32:52 +0100

    Add flatlist content with cities

--- App.js
+++ App.js
@@ -1,13 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
+import { FlatList, Platform, SafeAreaView, StatusBar, StyleSheet, Text, View } from 'react-native';
+
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
+const cities = [
+  { id: 1, name: 'New York', country: 'United States' },
+  { id: 2, name: 'London', country: 'England' },
+  { id: 3, name: 'Berlin', country: 'Germany' },
+  { id: 4, name: 'Paris', country: 'France' },
+  { id: 5, name: 'Madrid', country: 'Spain' },
+  { id: 6, name: 'Rome', country: 'Italy' },
+  { id: 7, name: 'Brussels', country: 'Belgium' },
+  { id: 8, name: 'Lisbonne', country: 'Portugal' }
+]
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <StatusBar barStyle='dark-content' backgroundColor='#fff' />
+      <FlatList
+        data={cities}
+        renderItem={({ item }) => (
+          <View style={styles.item}>
+            <Text style={styles.textName}>{item.name}</Text>
+            <Text style={styles.textCountry}>{item.country}</Text>
+          </View>
+        )}
+        keyExtractor={item => item.id.toString()}
+      />
+    </SafeAreaView>
   );
 }
 
@@ -17,5 +37,20 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: Platform.OS === 'ios' ? 0 : StatusBar.currentHeight,
   },
+  item: {
+    marginVertical: 10,
+    borderWidth: 1,
+    alignItems: 'center',
+    padding: 10,
+    borderRadius: 10,
+  },
+  textName: {
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+  textCountry: {
+    fontSize: 14,
+  }
 });
