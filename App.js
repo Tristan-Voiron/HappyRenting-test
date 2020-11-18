faf5990580652538d39a7fb21ea9233861fc8455
Tristan-Voiron <tristan.voiron@gmail.com>
2020-11-18 09:23:33 +0100

    Try using MultiSelect but not working as intended yet

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
-import { FlatList, Platform, SafeAreaView, StatusBar, StyleSheet, Text, TouchableOpacity } from 'react-native';
+import { FlatList, Platform, SafeAreaView, StatusBar, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import React, { Component } from 'react';
 
-import React from 'react';
+import MultiSelect from 'expo-multiple-select';
 
 const cities = [
   { id: 1, name: 'New York', country: 'United States' },
@@ -13,22 +14,55 @@ const cities = [
   { id: 8, name: 'Lisbonne', country: 'Portugal' }
 ]
 
-export default function App() {
-  return (
-    <SafeAreaView style={styles.container}>
-      <StatusBar barStyle='dark-content' backgroundColor='#fff' />
-      <FlatList
-        data={cities}
-        renderItem={({ item }) => (
-          <TouchableOpacity style={styles.item} onPress={() => console.warn(item.name)}>
-            <Text style={styles.textName}>{item.name}</Text>
-            <Text style={styles.textCountry}>{item.country}</Text>
-          </TouchableOpacity>
-        )}
-        keyExtractor={item => item.id.toString()}
-      />
-    </SafeAreaView>
-  );
+export default class App extends Component {
+  state = {
+    selectedItems : [],
+  };
+
+  onSelectedItemsChange = selectedItems => {
+    this.setState({ selectedItems });
+  };
+
+  render () {
+    return (
+      <SafeAreaView style={styles.container}>
+        <StatusBar barStyle='dark-content' backgroundColor='#fff' />
+
+        <MultiSelect
+          hideTags
+          items={cities}
+          uniqueKey="id"
+          ref={(component) => { this.multiSelect = component }}
+          onSelectedItemsChange={this.onSelectedItemsChange}
+          selectedItems={this.selectedItems}
+          selectText="Pick Items"
+          searchInputPlaceholderText="Search Items..."
+          onChangeInput={ (text)=> console.log(text)}
+          tagRemoveIconColor="#CCC"
+          tagBorderColor="#CCC"
+          tagTextColor="#CCC"
+          selectedItemTextColor="#CCC"
+          selectedItemIconColor="#CCC"
+          itemTextColor="#000"
+          displayKey="name"
+          searchInputStyle={{ color: '#CCC' }}
+          submitButtonColor="#CCC"
+          submitButtonText="Submit"
+        />
+
+        <FlatList
+          data={cities}
+          renderItem={({ item }) => (
+            <TouchableOpacity style={styles.item} onPress={() => console.warn(item.name)}>
+              <Text style={styles.textName}>{item.name}</Text>
+              <Text style={styles.textCountry}>{item.country}</Text>
+            </TouchableOpacity>
+          )}
+          keyExtractor={item => item.id.toString()}
+        />
+      </SafeAreaView>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
